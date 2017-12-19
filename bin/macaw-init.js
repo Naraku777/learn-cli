a75eac04a458f158db9c3a5d30347714fcfe0620
naraku777 <442815900@qq.com>
2017-12-19 18:42:37 +0800

    Add init code

--- bin/macaw-init.js
+++ bin/macaw-init.js
@@ -1,6 +1,11 @@
-//
+#! /usr/bin/env node
+
+// 👆 is [Shebang](http://smilejay.com/2012/03/linux_shebang/)
 
 const program = require('commander')
+const path = require('path')
+const fs = require('fs')
+const glob = require('glob')
 
 program
   .usage('<project-name>')
@@ -11,12 +16,34 @@ let projectName = program.args[0]
 
 // Project name is required
 if (!projectName) {
+  // As same as run --help
   program.help()
   return
+}
+
+// Traverse current directory
+const list = glob.sync('*')
+let rootName = path.basename(process.cwd())
+// If not empty directory
+if (list.length) {
+  if (list.filter((name) => {
+    const fileName = path.resolve(process.cwd(), path.join('.', name))
+    const isDir = fs.statSync(fileName).isDirectory()
+    return name.indexOf(projectName) !== -1 && isDir
+  }).length !== 0) {
+    console.log(`Project ${projectName} is existed`)
+    return
+  }
+  rootName = projectName
+} else if (rootName === projectName) {
+  rootName = '.'
+} else {
+  rootName = projectName
 }
 
 go()
 
 function go () {
   // placeholder, handle child command
+  console.log(path.resolve(process.cwd(), path.join('.', rootName)))
 }
